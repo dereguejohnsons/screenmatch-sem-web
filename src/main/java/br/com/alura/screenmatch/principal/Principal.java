0577fe0eaf7357b22c9a721a5ce801162ef17c35
dereguejohnsons <dereguejohnsons632@gmail.com>
2026-04-21 10:45:40 -0300

    adc totaldo modulo 2

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,20 +1,23 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoAPI;
 import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
+    private ConsumoAPI consumidorAPI = new ConsumoAPI();
+    private ConverteDados conversor = new ConverteDados();
 
     //Constantes
     private final String ENDERECO_BUSCA = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apiKey=351cada8";
-
-    private ConsumoAPI consumidorAPI = new ConsumoAPI();
-    private ConverteDados conversor = new ConverteDados();
 
      public void exibeMenu(){
 
@@ -23,5 +26,16 @@ public class Principal {
          var json = consumidorAPI.obterDados(ENDERECO_BUSCA + nomeSerie.replace(" ", "+") + API_KEY);
          DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
          System.out.println(dados);
+
+         List<DadosTemporada> temporadas = new ArrayList<>();
+         for ( int i = 1; i <= dados.totalTemporadas(); i++){
+             json = consumidorAPI.obterDados(ENDERECO_BUSCA + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+             DadosTemporada dadosTemporada  = conversor.obterDados(json, DadosTemporada.class);
+             temporadas.add(dadosTemporada);
+         }
+
+         temporadas.forEach(t -> t.episodios().forEach( e -> System.out.println("Título: " + e.titulo() + " /" +
+                 " Avaliação: " + e.avaliacao() + " /" +
+                 " Lançado em: " + e.dataLancamento())));
      }
 }
