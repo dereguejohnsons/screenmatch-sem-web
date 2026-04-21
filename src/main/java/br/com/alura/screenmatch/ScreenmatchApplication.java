ecfa179616dff99897c1fd437ee0b264a0341ace
dereguejohnsons <dereguejohnsons632@gmail.com>
2026-04-21 10:12:59 -0300

    adc record temporadas

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,18 +1,9 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosEpisodio;
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.service.ConsumoAPI;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -23,22 +14,10 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Primeiro projeto Spring sem Web");
 
-		//"https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&plot=full&apiKey=351cada8"
+		Principal principal = new Principal();
+		principal.exibeMenu();
 
-
-		var consumidorAPI = new ConsumoAPI();
-		var json = consumidorAPI.obterDados("https://www.omdbapi.com/?t=breaking+bad&plot=full&apiKey=351cada8");
-		System.out.println(json);
-		/*json = consumidorAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);*/
-        var conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-		json = consumidorAPI.obterDados("https://www.omdbapi.com/?t=breaking+bad&season=3&episode=10&plot=full&apiKey=351cada8");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
 	}
 
 
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
 
 import java.util.Scanner;
 
@@ -12,14 +14,14 @@ public class Principal {
     private final String API_KEY = "&apiKey=351cada8";
 
     private ConsumoAPI consumidorAPI = new ConsumoAPI();
-
+    private ConverteDados conversor = new ConverteDados();
 
      public void exibeMenu(){
 
          System.out.println("Digite o nome da série para busca:");
          var nomeSerie = leitura.nextLine();
-
          var json = consumidorAPI.obterDados(ENDERECO_BUSCA + nomeSerie.replace(" ", "+") + API_KEY);
-
+         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+         System.out.println(dados);
      }
 }
